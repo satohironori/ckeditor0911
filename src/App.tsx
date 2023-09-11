3c3637ff6e84e2afb95dff335998f298ea3085b1
root <root@SME180050013.aeonpeople1.aeonpeople.biz>
2023-09-11 13:31:43 +0900

    ckeditor37単体をインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+//import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import Editor from '@ckeditor/ckeditor5-build-classic';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => { 
+       return (
+           <div className="App">
+               <h2>Using CKEditor 5 build in React</h2>
+               <CKEditor
+                   editor={ Editor }
+                   data="<p>Hello from CKEditor 5!</p>"
+                   onReady={ (editor: any) => {
+                       // You can store the "editor" and use when it is needed.
+                       console.log( 'Editor is ready to use!', editor );
+                   } }
+                   onChange={ ( event: any, editor: { getData: () => any; } ) => {
+                       const data = editor.getData();
+                       console.log( { event, editor, data } );
+                   } }
+                   onBlur={ ( event: any, editor: any ) => {
+                       console.log( 'Blur.', editor );
+                   } }
+                   onFocus={ ( event: any, editor: any ) => {
+                       console.log( 'Focus.', editor );
+                   } }
+               />
+           </div>
+       );   
 }
-
 export default App;
